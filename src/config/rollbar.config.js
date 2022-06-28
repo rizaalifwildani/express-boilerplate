f84390ee694965bc56378818f550a48988fed05e
Riza Alif Wildani <rizaalifofficial@gmail.com>
2022-06-29 04:22:57 +0700

    Rollbar logging for Scheduler

--- src/config/rollbar.config.js
+++ src/config/rollbar.config.js
@@ -5,6 +5,9 @@ const rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  payload: {
+    code_version: '1.0.0',
+  },
 })
 
 module.exports = rollbar
--- src/scheduler/backup-db.scheduler.js
+++ src/scheduler/backup-db.scheduler.js
@@ -1,11 +1,13 @@
 const cron = require('node-cron')
 const exec = require('child_process').exec
+const rollbar = require('../config/rollbar.config')
 
 const backupDBScheduler = cron.schedule('0 0 * * *', () => {
   console.log('dumping . . .')
   exec('mysqldump -u root db_express > db_express_dump.sql', (error) => {
     if (error) {
       console.log('dumping error : ', error.message)
+      rollbar.error(`Scheduler - backup-db.scheduler.js : ${error.message}`)
     } else {
       console.log('dumping finish')
     }
